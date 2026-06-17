22e1003f0e5cd4f419458b7bd386f5fd71825be1
lindabell001 <61391300+lindabell001@users.noreply.github.com>
2026-06-17 10:48:33 -0400

    Add dynamic chat room route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Mission from './pages/Mission';
 import Tavern from './pages/tavern';
 import Locations from './pages/tavern/locations';
 import Issues from './pages/tavern/issues';
+import ChatRoomPage from './pages/tavern/chat/[slug]';   // Dynamic chat room
 
 const App = () => {
   return (
@@ -29,6 +30,9 @@ const App = () => {
           <Route path="/tavern" element={<Tavern />} />
           <Route path="/tavern/locations" element={<Locations />} />
           <Route path="/tavern/issues" element={<Issues />} />
+          
+          {/* Dynamic Chat Room - This is the important one */}
+          <Route path="/tavern/chat/:slug" element={<ChatRoomPage />} />
 
           {/* 404 Catch-all */}
           <Route path="*" element={<div>Page Not Found</div>} />
