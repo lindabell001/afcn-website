4408e04519b46f43c5e6331837bdf2154661df91
lindabell001 <61391300+lindabell001@users.noreply.github.com>
2026-06-17 15:36:47 -0400

    Add new routes for Tavern system pages
    adding issues n locations to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import BecomeOne from './pages/BecomeOne';
 import Donate from './pages/Donate';
 import Mission from './pages/Mission';
 import Tavern from './pages/tavern';
+import Locations from './pages/tavern/locations';
+import Issues from './pages/tavern/issues';
+import ChatRoomPage from './pages/tavern/chat/[slug]';
 import CommitteesOfObservation from './pages/CommitteesOfObservation';
 
 const App = () => {
@@ -24,8 +27,13 @@ const App = () => {
           <Route path="/donate" element={<Donate />} />
           <Route path="/mission" element={<Mission />} />
 
-          {/* Main Hubs */}
+          {/* Tavern System */}
           <Route path="/tavern" element={<Tavern />} />
+          <Route path="/tavern/locations" element={<Locations />} />
+          <Route path="/tavern/issues" element={<Issues />} />
+          <Route path="/tavern/chat/:slug" element={<ChatRoomPage />} />
+
+          {/* Committees of Observation */}
           <Route path="/committees-of-observation" element={<CommitteesOfObservation />} />
 
           <Route path="*" element={<div>Page Not Found</div>} />
