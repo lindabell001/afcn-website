b888e470586df055eef54dbe8fa52868fc31cbf4
lindabell001 <61391300+lindabell001@users.noreply.github.com>
2026-06-18 15:24:32 -0400

    Re-enable SiteHeader in SiteLayout component

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -4,9 +4,7 @@ import SiteFooter from "./SiteFooter";
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col bg-background">
-      {/* TEMPORARY: Header disabled to test where old text is coming from */}
-      {/* <SiteHeader /> */}
-
+      <SiteHeader />
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
