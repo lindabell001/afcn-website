e81619208be1bba14491f49caf5758ada9953575
lindabell001 <61391300+lindabell001@users.noreply.github.com>
2026-06-18 15:21:15 -0400

    Comment out SiteHeader in SiteLayout component
    Temporarily disable SiteHeader for testing purposes.

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -4,7 +4,9 @@ import SiteFooter from "./SiteFooter";
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col bg-background">
-      <SiteHeader />
+      {/* TEMPORARY: Header disabled to test where old text is coming from */}
+      {/* <SiteHeader /> */}
+
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
