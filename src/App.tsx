e45cad21553ebffe5298035656a373f7b4f24398
lindabell001 <61391300+lindabell001@users.noreply.github.com>
2026-06-19 13:32:59 -0400

    Add ScrollToTop component and update routes
    Make every page navigation scroll to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import SiteLayout from '@/components/SiteLayout';
 
 // Page Imports
@@ -9,17 +10,30 @@ import Resources from './pages/Resources';
 import BecomeOne from './pages/BecomeOne';
 import Donate from './pages/Donate';
 import Mission from './pages/Mission';
+
+// Tavern & Committees
 import Tavern from './pages/tavern';
 import Locations from './pages/tavern/locations';
 import Issues from './pages/tavern/issues';
 import ChatRoomPage from './pages/tavern/chat/[slug]';
 import CommitteesOfObservation from './pages/CommitteesOfObservation';
 import LocalCommittees from './pages/committees/local';
-import CommitteesIssues from './pages/committees/issues';   // ← New
+import CommitteesIssues from './pages/committees/issues';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [pathname]);
+
+  return null;
+}
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <SiteLayout>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -29,13 +43,14 @@ const App = () => {
           <Route path="/donate" element={<Donate />} />
           <Route path="/mission" element={<Mission />} />
 
-          {/* Main Hubs */}
+          {/* Tavern Routes */}
           <Route path="/tavern" element={<Tavern />} />
-          <Route path="/committees-of-observation" element={<CommitteesOfObservation />} />
-
-          {/* List Pages */}
           <Route path="/tavern/locations" element={<Locations />} />
           <Route path="/tavern/issues" element={<Issues />} />
+          <Route path="/tavern/chat/:slug" element={<ChatRoomPage />} />
+
+          {/* Committees Routes */}
+          <Route path="/committees-of-observation" element={<CommitteesOfObservation />} />
           <Route path="/committees/local" element={<LocalCommittees />} />
           <Route path="/committees/issues" element={<CommitteesIssues />} />
 
