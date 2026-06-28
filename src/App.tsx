8c89fed562d184d39cecf04efd0158a8643e752d
lindabell001 <61391300+lindabell001@users.noreply.github.com>
2026-06-28 13:22:54 -0400

    Clean up comments in App.tsx
    Removed comments for better code clarity and organization. We did this exact thing before

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,24 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import { useLayoutEffect } from 'react';
 import SiteLayout from '@/components/SiteLayout';
 
-// Main Stable Pages
 import Index from './pages/Index';
 import About from './pages/About';
 import Resources from './pages/Resources';
 import BecomeOne from './pages/BecomeOne';
 import Donate from './pages/Donate';
 import Mission from './pages/Mission';
-
-// Core Secondary Pages
 import Tavern from './pages/tavern';
 import CommitteesOfObservation from './pages/CommitteesOfObservation';
-
-// Constitution Academy
 import ConstitutionAcademy from './pages/ConstitutionAcademy';
-
-// Footer Pages
 import PrivacyAndData from './pages/PrivacyAndData';
 import SocialWelfareOrganization from './pages/SocialWelfareOrganization';
 import TransparencyAndOperations from './pages/TransparencyAndOperations';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-
   useLayoutEffect(() => {
     window.scrollTo({ top: 0, behavior: 'instant' });
   }, [pathname]);
-
   return null;
 }
 
@@ -45,20 +36,13 @@ const App = () => {
           <Route path="/become-one" element={<BecomeOne />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="/mission" element={<Mission />} />
-
-          {/* Constitution Academy */}
           <Route path="/resources/constitution-academy" element={<ConstitutionAcademy />} />
-
-          {/* Footer Pages */}
           <Route path="/privacy-and-data" element={<PrivacyAndData />} />
           <Route path="/social-welfare-organization" element={<SocialWelfareOrganization />} />
           <Route path="/transparency-and-operations" element={<TransparencyAndOperations />} />
-
-          {/* Tavern & Committees */}
           <Route path="/tavern" element={<Tavern />} />
           <Route path="/committees" element={<CommitteesOfObservation />} />
           <Route path="/committees-of-observation" element={<CommitteesOfObservation />} />
-
           <Route path="*" element={<div className="text-center py-20 text-xl">Page Not Found</div>} />
         </Routes>
       </SiteLayout>
